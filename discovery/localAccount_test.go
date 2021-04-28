00db0907f958a4b1c3877677ca509a3ffde2eb24
bluesentinelsec <bluesentinel@protonmail.com>
2021-04-28 18:17:09 -0400

    Updated TestGetCurrentUser for Windows OS

--- discovery/localAccount_test.go
+++ discovery/localAccount_test.go
@@ -2,18 +2,27 @@ package discovery
 
 import (
 	"os/exec"
+	"runtime"
 	"strings"
 	"testing"
 )
 
 func TestGetCurrentUser(t *testing.T) {
+	// get current user through a shell command
 	cmd := exec.Command("whoami")
 	cmdOutput, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Errorf("Error running cmd.CombinedOutput: %q", err)
 	}
-	currentUser := strings.TrimSuffix(string(cmdOutput), "\n")
+	// trim carriage return and new line characters
+	var currentUser string
+	if runtime.GOOS == "windows" {
+		currentUser = strings.TrimSuffix(string(cmdOutput), "\n\r")
+	} else {
+		currentUser = strings.TrimSuffix(string(cmdOutput), "\n")
+	}
 
+	// run the test
 	want := string(currentUser)
 	got, err := GetCurrentUser()
 	if err != nil {
